ac266aaeb921516ede0079b97498e2c05aef54b5
UllasUKrishnan <krishnanullas3@gmail.com>
2021-11-09 13:16:02 +0530

    Implemented the second method

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
-// Method 1 to implement counter function
-const decrease = document.querySelector('.decrease');
-const reset = document.querySelector('.reset');
-const increase = document.querySelector('.increase');
-let value = document.querySelector('#value');
+/**============================================
+ *               METHOD 1
+ *=============================================**/
 
-decrease.addEventListener('click', function (e) {
-  let newValue = Number(value.textContent);
-  newValue--;
-  value.textContent = newValue;
-});
+// const decrease = document.querySelector('.decrease');
+// const reset = document.querySelector('.reset');
+// const increase = document.querySelector('.increase');
+// let value = document.querySelector('#value');
 
-reset.addEventListener('click', () => {
-  value.textContent = 0;
-});
+// decrease.addEventListener('click', function (e) {
+//   let newValue = Number(value.textContent);
+//   newValue--;
+//   value.textContent = newValue;
+// });
 
-increase.addEventListener('click', () => {
-  let newValue = Number(value.textContent);
-  newValue++;
-  value.textContent = newValue;
+// reset.addEventListener('click', () => {
+//   value.textContent = 0;
+// });
+
+// increase.addEventListener('click', () => {
+//   let newValue = Number(value.textContent);
+//   newValue++;
+//   value.textContent = newValue;
+// });
+
+/**============================================
+ *               METHOD 2
+ *=============================================**/
+
+let count = 0;
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('increase')) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = 'green';
+    } else if (count < 0) {
+      value.style.color = 'red';
+    } else {
+      value.style.color = 'black';
+    }
+    value.textContent = count;
+  });
 });
